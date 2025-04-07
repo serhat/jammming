313cd37df4794f86fc164563bbcdb5c8ea353bad
serhat bingölbali <serhatbnglbl184@gmail.com>
2025-04-07 09:35:40 +0300

    SearchBar bileşeni eklendi

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+function SearchBar() {
+    return (
+        <div className="SearchBar">
+            <input placeholder="Enter A Song, Album, or Artist" />
+            <button>SEARCH</button>
+
+        </div>
+    );
+}
+export default SearchBar;
